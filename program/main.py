c98e1b92a88949e2a819f1274bffa2b2722b4036
alexeiepif <aled2093746@gmail.com>
2023-09-25 11:26:44 +0300

    main

--- program/main.py
+++ program/main.py
@@ -1,37 +1,33 @@
 import random as rnd
-from statistics import correlation
+
 import matplotlib.pyplot as plt
 import numpy as np
 import timeit
 
 
-def findmin(a, maxitem, len):
-    min = maxitem
-    for i in range(len):
-        if min > a[i]:
-            min = a[i]
+def findmin():
+    min = randmax
+    for i in a:
+        if min > i:
+            min = i
     return min
 
-def findmax(a, len):
+
+def findmax():
     max = 0
-    for i in range(len):
-        if max < a[i]:
-            max = a[i]
+    for i in a:
+        if max < i:
+            max = i
     return max
 
 
 def create_graph(b, c, aur, bur):
-    y_values = np.linspace(0, max(c), num=5)
-    x_values = np.linspace(0, b[-1], num=11)
     plt.scatter(b, c, s=5)
-
     y_line = aur * np.array(b) + bur
     plt.plot(b, y_line, color='red')
     plt.title("График")
     plt.xlabel("X-ось")
     plt.ylabel("Y-ось")
-    plt.xticks(x_values)
-    plt.yticks(y_values)
     correlation_coefficient = np.corrcoef(c, b)[0, 1]
     return correlation_coefficient
 
@@ -39,28 +35,25 @@ def create_graph(b, c, aur, bur):
 correlation_v = []
 # Цикл нужен для создания двух графиков, один при среднем случае, второй при худшем
 for namegraph in ["Минимум", "Максимум"]:
-    x = []
+    x = [i for i in range(10, 10001, 10)]
     time = []
     x2 = []
     xtime = []
     randmax = 1000000
-    for i in range(10, 10001, 10):
-        x.append(i)
-        a = [rnd.randint(0, randmax) for j in range(i)]
-        if namegraph == "Минимум":
-            timer = timeit.timeit(lambda: findmin(a, randmax, i), number=1)
-        else:
-            timer = timeit.timeit(lambda: findmin(a, randmax, i), number=1)
-        time.append(timer)
+    if namegraph == "Минимум":
+        for i in x:
+            a = [rnd.randint(0, randmax) for j in range(i)]
+            time.append(timeit.timeit(lambda: findmin(), number=10)/10)
+    else:
+        for i in x:
+            a = [rnd.randint(0, randmax) for j in range(i)]
+            time.append(timeit.timeit(lambda: findmax(), number=10)/10)
 
-    for i, j in zip(x, time):
-        x2.append(i**2)
-        xtime.append(i*j)
     # Вычисление коэффицентов в системе уравнений метода наименьших квадратов
     sx = sum(x)
     stime = sum(time)
-    sx2 = sum(x2)
-    sxtime = sum(xtime)
+    sx2 = sum(i**2 for i in x)
+    sxtime = sum(i*j for i, j in zip(x, time))
     n = len(x)
     # k - это коэффициент, при котором вычитание
     # из первого уравнения второго,
